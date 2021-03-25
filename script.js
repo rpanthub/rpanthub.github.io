3cfc6cd13f15d56a6aba2f4b19c5d5c7d8ca6118
rpanthub <ranjan.pant@gmail.com>
2021-03-25 00:48:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+ var todaydate = new Date();
+ var month = todaydate.getMonth() + 1;
+ var day = todaydate.getDate();
+ var year = todaydate.getFullYear();
+ var datestring = month + "/" + day + "/" + year;
+ window.onload = function(){
+ document.getElementById("date").value = datestring;
+ }
 
-document.getElementById('demo').value= new Date();
 
