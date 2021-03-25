50dfc42833de36d407d7719234fa099142d0204b
rpanthub <ranjan.pant@gmail.com>
2021-03-25 00:49:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  var todaydate = new Date();
- var month = todaydate.getMonth() + 1;
+ var month = todaydate.getMonth();
  var day = todaydate.getDate();
  var year = todaydate.getFullYear();
  var datestring = month + "/" + day + "/" + year;
