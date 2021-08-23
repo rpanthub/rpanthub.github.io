6c116d2039aa56aac52727c4aae12210781534f1
rpanthub <ranjan.pant@gmail.com>
2021-08-22 23:35:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 
 function displayStaticClock() {
  var todaydate = new Date();
- var month = todaydate.getMonth() + 1;
- var day = todaydate.getDate();
- var year = todaydate.getFullYear();
- var datestring = month + "/" + day + "/" + year;
- document.getElementById("date").innerHTML = "Today is " + todaydate;
+ //var month = todaydate.getMonth() + 1;
+ //var day = todaydate.getDate();
+ //var year = todaydate.getFullYear();
+ //var datestring = month + "/" + day + "/" + year;
+ if(typeof elem !== 'undefined' && elem !== null) {
+   elem.innerHTML = "Today is " + todaydate;
+ }
 }
 
 function displayClock(){
   var display = new Date().toLocaleTimeString();
-  document.getElementById("date").innerHTML = display;
+  if(typeof elem !== 'undefined' && elem !== null) {
+   elem.innerHTML = display;
+  }
   setTimeout(displayClock, 1000); 
 }
-
+var elem = document.getElementById("date");
 window.onload = displayStaticClock();
 
 
